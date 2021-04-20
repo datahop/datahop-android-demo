5290ae05c08259fa49254577cfd3c9fc8c09d895
asabya <asabya@gmail.com>
2021-04-20 15:09:51 +0530

    ipv4 only

--- app/src/main/java/com/example/datahopdemo/MainActivity.java
+++ app/src/main/java/com/example/datahopdemo/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("-----Version :", Datahop.version());
         try {
             Datahop.init(getApplicationContext().getCacheDir()+"/"+root);
         } catch (Exception e) {
