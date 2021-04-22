05aeaa23c38345369078224494ac59844b042eec
asabya <asabya@gmail.com>
2021-04-22 14:37:04 +0530

    mDNS working

--- app/src/main/java/com/example/datahopdemo/MainActivity.java
+++ app/src/main/java/com/example/datahopdemo/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 import datahop.Datahop;
+import datahop.Hook;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Hook {
 
     private static final String root = ".datahop";
+    ArrayList<String> activePeers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("-----Version :", Datahop.version());
         try {
-            Datahop.init(getApplicationContext().getCacheDir()+"/"+root);
+            Datahop.init(getApplicationContext().getCacheDir()+"/"+root, this);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Log.d("Node Id", Datahop.getID());
         Log.d("Node Status onCreate", String.valueOf(Datahop.isNodeOnline()));
         if (!Datahop.isNodeOnline()) {
             try {
@@ -46,5 +50,17 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void peerConnected(String s) {
+        Log.d("*** Peer Connected ***", s);
+        activePeers.add(s);
+    }
+
+    @Override
+    public void peerDisconnected(String s) {
+        Log.d("* Peer Disconnected *", s);
+        activePeers.remove(s);
     }
 }
