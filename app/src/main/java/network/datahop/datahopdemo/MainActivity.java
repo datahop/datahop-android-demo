f53581be918f8a8ed6a6fd009cb7662e4c26f0e8
asabya <asabya@gmail.com>
2021-06-03 08:58:11 +0530

    add discovery changes

--- app/src/main/java/network/datahop/datahopdemo/MainActivity.java
+++ app/src/main/java/network/datahop/datahopdemo/MainActivity.java
@@ -1,20 +1,12 @@
 package network.datahop.datahopdemo;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Random;
 
 import datahop.Datahop;
 import datahop.ConnectionManager;
@@ -22,17 +14,11 @@ import network.datahop.blediscovery.BLEAdvertising;
 import network.datahop.blediscovery.BLEServiceDiscovery;
 import network.datahop.wifidirect.WifiDirectHotSpot;
 import network.datahop.wifidirect.WifiLink;
-//import wifidriver.WifiConnection;
-//import wifidriver.WifiConnectionNotifier;
 
 public class MainActivity extends AppCompatActivity implements ConnectionManager {
 
     private static final String root = ".datahop";
-    private static final String TAG = MainActivity.class.getSimpleName();
     ArrayList<String> activePeers = new ArrayList<>();
-
-    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
-    private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,25 +27,30 @@ public class MainActivity extends AppCompatActivity implements ConnectionManager
         Log.d("-----Version :", Datahop.version());
         try {
             BLEServiceDiscovery bleDiscoveryDriver = BLEServiceDiscovery.getInstance(getApplicationContext());
-
             BLEAdvertising bleAdvertisingDriver = BLEAdvertising.getInstance(getApplicationContext());
 
             WifiDirectHotSpot hotspot = WifiDirectHotSpot.getInstance(getApplicationContext());
             WifiLink connection = WifiLink.getInstance(getApplicationContext());
 
-            Datahop.init(getApplicationContext().getCacheDir() + "/" + root, this, bleDiscoveryDriver, bleAdvertisingDriver, hotspot,connection);
+            Datahop.init(
+                    getApplicationContext().getCacheDir() + "/" + root,
+                    this,
+                    bleDiscoveryDriver,
+                    bleAdvertisingDriver,
+                    hotspot,
+                    connection
+            );
 
-            bleAdvertisingDriver.setNotifier(Datahop.getBleAdvNotifier());
-            bleDiscoveryDriver.setNotifier(Datahop.getBleDiscNotifier());
+            bleAdvertisingDriver.setNotifier(Datahop.getAdvertisementNotifier());
+            bleDiscoveryDriver.setNotifier(Datahop.getDiscoveryNotifier());
             hotspot.setNotifier(Datahop.getWifiHotspotNotifier());
             connection.setNotifier(Datahop.getWifiConnectionNotifier());
-
+            Datahop.startDiscovery();
             Datahop.start();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Log.d("Node Id", Datahop.peerInfo());
+        Log.d("Node Id", Datahop.id());
         Log.d("Node Status onCreate", String.valueOf(Datahop.isNodeOnline()));
         if (!Datahop.isNodeOnline()) {
             try {
@@ -68,108 +59,22 @@ public class MainActivity extends AppCompatActivity implements ConnectionManager
                 e.printStackTrace();
             }
         }
-
-        requestForPermissions();
-
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        Log.d(TAG, "Permissions " + requestCode + " " + permissions + " " + grantResults);
-        switch (requestCode) {
-            case PERMISSION_REQUEST_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Log.d(TAG, "Location accepted");
-                    //timers.setLocationPermission(true);
-                    //if(timers.getStoragePermission())startService();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Log.d(TAG, "Location not accepted");
-
-                }
-                break;
-            }
-            case PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE:
-
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Log.d(TAG, "Storage accepted");
-                    //timers.setStoragePermission(true);
-                    //new CreateWallet(getApplicationContext()).execute();
-                    //if(timers.getLocationPermission())startService();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    //G.Log(TAG,"Storage not accepted");
-
-                }
-        }
-
-        // other 'case' lines to check for other
-        // permissions this app might request.
-
-    }
-
-    private void requestForPermissions() {
-        if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_FINE_LOCATION);
-                }
-            });
-            builder.show();
-        }
-
-
-    }
-
 
     public void onStart() {
         super.onStart();
         Log.d("Node Status onStart", String.valueOf(Datahop.isNodeOnline()));
         try {
-            String Id = Datahop.peerInfo();
+            String Id = Datahop.id();
             final TextView textViewID = this.findViewById(R.id.textview_id);
             textViewID.setText(Id);
 
-            /*String addrs = Datahop.getAddress();
+            String addrs = Datahop.addrs();
             final TextView textViewAddrs = this.findViewById(R.id.textview_address);
-            textViewAddrs.setText(addrs);*/
-
-            final Button button = findViewById(R.id.refreshbutton);
-            button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    byte[] array = new byte[7]; // length is bounded by 7
-                    new Random().nextBytes(array);
-                    String generatedString = new String(array, Charset.forName("UTF-8"));
-                    //final TextView text = findViewById(R.id.textView);
-                    //text.setText(generatedString);
-                    Log.d(TAG, "Refresh status " + generatedString);
-                    // Code here executes on main thread after user presses button
-                    Datahop.updateTopicStatus("topic1", generatedString.getBytes());
-                }
-            });
+            textViewAddrs.setText(addrs);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    protected void onStop() {
-        Datahop.stop();
-
-        super.onStop();
     }
 
     @Override
@@ -183,7 +88,4 @@ public class MainActivity extends AppCompatActivity implements ConnectionManager
         Log.d("* Peer Disconnected *", s);
         activePeers.remove(s);
     }
-
-
-
 }
